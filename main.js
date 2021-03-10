e3d509b3fdb777b27e1302f49d232e90bedabaa9
Josh Whitwell <josh.whitwell@gmail.com>
2021-03-10 14:50:50 -0500

    objects

--- main.js
+++ main.js
@@ -1,3 +1,86 @@
-function myTest() {
-    console.log("test")
+function Cat(name) {
+    this.name = name;
 }
+
+Cat.prototype = {
+    constructor: Cat,
+};
+
+function Bear(name) {
+    this.name = name;
+}
+
+Bear.prototype = {
+    constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom")
+    }
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype)
+
+let beagle = new Dog();
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird
+Dog.prototype.constructor = Dog
+
+
+let duck = new Bird();
+let beagle = new Dog();
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype.constructor = Dog
+Dog.prototype.bark = function() {
+  console.log("Woof!")
+}
+
+
+// Only change code above this line
+
+let beagle = new Dog();
